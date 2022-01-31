a18acf4ed49a41591143f3d2948525bbd1c19b97
hellozo0 <ahju0011@gmail.com>
2022-01-31 13:09:52 +0900

    [feat] 2,3강 반복하기

--- CODE_LION/PYTHON/codelion.py
+++ CODE_LION/PYTHON/codelion.py
@@ -1,4 +1,11 @@
 import random
+import time
 
-print(random.choice(["된장찌개", "피자", "제육볶음"]))
+for i in range(30):
+    print(random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"]))
+    time.sleep(1)
 
+while True:
+    print(random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"]))
+    time.sleep(1)
+    break
