3b8d4ad5a801dab8ebae11f78b5f4eaf92bb47d5
hellozo0 <ahju0011@gmail.com>
2022-02-01 02:58:11 +0900

    [feat] Ch5. 메일 최종 파일

--- CODE_LION/PYTHON/mail.py
+++ CODE_LION/PYTHON/mail.py
@@ -1,32 +1,34 @@
-from email.message import EmailMessage
 import smtplib
+from email.message import EmailMessage
+import imghdr
+import re
 
-# SMTP 접속을 위한 서버, 계정 설정
 SMTP_SERVER = "smtp.gmail.com"
-# google의 SMTP server 포트 주소는 465
 SMTP_PORT = 465
 
-
-# 이메일 유효성 검사 함수
-def is_valid(addr):
-    import re
-    if re.match('(^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,3}$)', addr):
-        return True
+def sendEmail(addr):
+    reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+    if bool(re.match(reg,addr)):
+        smtp.send_message(message)
+        print("정상적으로 메일이 발송되었습니다.")
     else:
-        return False
+        print("유효한 이메일 주소가 아닙니다.")
 
 message = EmailMessage()
-message.set_content("코드라이언 메일링 수업 - 본문입니다.")
+message.set_content("코드라이언 수업중입니다.")
 
-message["Subject"] = "코드라이언 메일링 수업입니다."
+message["Subject"] = "이것은 제목입니다."
 message["From"] = "###@gmail.com"
 message["To"] = "###@gmail.com"
 
+with open("codelion.png","rb") as image:
+    image_file = image.read()
+
+image_type = imghdr.what('codelion',image_file)
+message.add_attachment(image_file,maintype='image',subtype=image_type)
+
 smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
 smtp.login("###@gmail.com","######")
-
-is_valid("###@gmail.com")
-if smtp.send_message(message)=={} :
-    print("성공적으로 메일을 보냈습니다.")
-
+# 메일을 보내는 sendEmail 함수를 호출해서 실행해보기
+sendEmail("###gmailcom")
 smtp.quit()
