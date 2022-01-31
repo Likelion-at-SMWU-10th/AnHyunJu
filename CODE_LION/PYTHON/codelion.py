59b93f9d74ad6aee7a839661bb092af357d2ce0d
hellozo0 <ahju0011@gmail.com>
2022-01-31 14:18:27 +0900

    [feat] set, timesleep 사용

--- CODE_LION/PYTHON/codelion.py
+++ CODE_LION/PYTHON/codelion.py
@@ -115,3 +115,19 @@ while True:
     
 print(lunch)
 set_lunch = set(lunch)
+
+while True:
+    print(set_lunch)
+    item = input("음식을 삭제해주세요(q를 입력하면 입력 종료) : ")
+    if item == "q":
+        break
+    else:
+        set_lunch = set_lunch - set([item])
+
+print(set_lunch,"중에서 선택합니다.")
+
+for i in range(5,0,-1):
+    print(i)
+    time.sleep(1)
+
+print(random.choice(list(set_lunch)))
