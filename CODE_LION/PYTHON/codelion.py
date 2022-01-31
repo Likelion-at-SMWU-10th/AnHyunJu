8b9c8d0972a52fb80c48a72cd17122e134f6e4f2
hellozo0 <ahju0011@gmail.com>
2022-01-31 13:14:44 +0900

    [feat] 4강 변수

--- CODE_LION/PYTHON/codelion.py
+++ CODE_LION/PYTHON/codelion.py
@@ -1,11 +1,17 @@
 import random
 import time
 
-for i in range(30):
-    print(random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"]))
-    time.sleep(1)
+# for i in range(30):
+#     print(random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"]))
+#     time.sleep(1)
 
-while True:
-    print(random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"]))
-    time.sleep(1)
-    break
+# while True:
+#     print(random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"]))
+#     time.sleep(1)
+#     break
+
+lunch = random.choice(["된장찌개", "피자", "제육볶음", "치킨", "떡볶이", "라면","감자튀김"])
+lunch = "냉장고"
+dinner = random.choice(["김밥", "쫄면", "돈까스"])
+
+print(lunch)
